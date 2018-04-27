86e8ec91a6791cecd3f0eb05f300d491b7b49e4c
Adam Fallon <afallon0@icloud.com>
2018-04-27 01:02:05 +0100

    Init commit

--- NoteToDEVONthink/ViewController.swift
+++ NoteToDEVONthink/ViewController.swift
@@ -1,25 +1,80 @@
-//
-//  ViewController.swift
-//  NoteToDEVONthink
-//
-//  Created by Adam Fallon on 26/04/2018.
-//  Copyright © 2018 Adam Fallon. All rights reserved.
-//
-
 import UIKit
+import Floaty
+
+class DEVONthinkNote {
+    var date : Date
+    var title : String?
+    var text : String?
+    
+    init( title : String?, text : String) {
+        self.date = Date()
+        self.title = title
+        self.text = text
+    }
+}
+
+class DEVONthinkURI {
+    let path : String
+    
+    init() {
+        self.path = "x-devonthink://createText"
+    }
+    
+    func url( note : DEVONthinkNote ) -> URL {
+        let queryItems = [NSURLQueryItem(name: "title", value: "{[\(note.title!)]}"),
+                          NSURLQueryItem(name: "location", value: "{[Inbox]}"),
+                          NSURLQueryItem(name: "text", value: "\(note.text!)")]
+        let urlComps = NSURLComponents(string: self.path)!
+        urlComps.queryItems = queryItems as [URLQueryItem]
+        return urlComps.url!
+    }
+}
+
+// Utils Class
+
+extension String {
+    var lines: [String] { return self.components(separatedBy: CharacterSet.newlines) }
+}
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createSendButton()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.textView.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func createSendButton() {
+        let floaty = Floaty()
+        floaty.addItem("Send to DEVONthink", icon: UIImage(named: "SendIcon"), handler: { item in
+            self.sendToDevonThink()
+        })
+        self.view.addSubview(floaty)
+    }
+    
+    func alterTextWhenNotEditingTitle() {
+        // Shrink down the font size.
+    }
+    
+    func sendToDevonThink() {
+        let textLines = textView.text.lines[1..<textView.text.lines.count]
+        let note : DEVONthinkNote = DEVONthinkNote(title: textView.text.lines[0] , text: textLines.joined(separator: "\n"))
+        
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(DEVONthinkURI().url(note: note), options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(DEVONthinkURI().url(note: note))
+        }
+    }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'NoteToDEVONthink' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NoteToDEVONthink
+    pod "Floaty", "~> 4.0.0" # Use version 4.0.0 for Swift 4.0
+end
